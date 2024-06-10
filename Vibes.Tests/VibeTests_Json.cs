e0b26292ce7d538011970fc7adbdefd15b514434
Than <thanthemannn@gmail.com>
2024-06-10 11:15:25 -0400

    Key Json Tests now use base class for smoother implementation

--- Vibes.Tests/VibeTests_Json.cs
+++ Vibes.Tests/VibeTests_Json.cs
@@ -2,11 +2,15 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Xunit;
 
-namespace Vibes.Core.Tests
+namespace Vibes.Core.Tests.Json
 {
-    public class VibeTests_Json
+    #region Keys
+
+    public class VibeTests_VibeKeyJson : BaseIVibeKeyTest<VibeKey> { public override VibeKey CreateKey(string key) => new(key); }
+    public class VibeTests_VibeKeyObjectJson : BaseIVibeKeyTest<VibeKeyObject> { public override VibeKeyObject CreateKey(string key) => new(key); }
+    public abstract class BaseIVibeKeyTest<T> where T : IVibeKey
     {
-        #region VibeKey
+        public abstract T CreateKey(string key);
 
         [Theory]
         [InlineData("vibekey")]
@@ -14,15 +18,15 @@ namespace Vibes.Core.Tests
         [InlineData(VibeKey.INVALID_KEY_NAME)]
         public void Test_VibeKeySerialization(string key)
         {
-            var preSerializedKey = new VibeKey(key);
+            var preSerializedKey = CreateKey(key);
             Assert.Equal(key, preSerializedKey.Name); //* name should match string
 
             string json = JsonConvert.SerializeObject(preSerializedKey);
             JObject jObject = JObject.Parse(json);
 
-            Assert.True(jObject.Count == 1, "VibeKey Json should only have one property: " + Json.NAME_KEY);
-            Assert.True(jObject[Json.NAME_KEY] != null, "VibeKey Json's property should be named: " + Json.NAME_KEY);
-            Assert.Equal(key, jObject[Json.NAME_KEY].ToString()); //*Name should be the same
+            Assert.True(jObject.Count == 1, "VibeKey Json should only have one property: " + Vibes.Json.NAME_KEY);
+            Assert.True(jObject[Vibes.Json.NAME_KEY] != null, "VibeKey Json's property should be named: " + Vibes.Json.NAME_KEY);
+            Assert.Equal(key, jObject[Vibes.Json.NAME_KEY].ToString()); //*Name should be the same
         }
 
         [Theory]
@@ -31,45 +35,12 @@ namespace Vibes.Core.Tests
         [InlineData(VibeKey.INVALID_KEY_NAME)]
         public void Test_VibeKeyDeserialization(string key)
         {
-            string json = JsonConvert.SerializeObject(new VibeKey(key));
-            var vibeKey = JsonConvert.DeserializeObject<VibeKey>(json);
+            string json = JsonConvert.SerializeObject(CreateKey(key));
+            var vibeKey = JsonConvert.DeserializeObject<T>(json);
 
             Assert.Equal(key, vibeKey.Name);
         }
-
-        #endregion
-
-        #region VibeKeyObject
-
-        [Theory]
-        [InlineData("vibekey")]
-        [InlineData("")]
-        [InlineData(VibeKey.INVALID_KEY_NAME)]
-        public void Test_VibeKeyObjectSerialization(string key)
-        {
-            var preSerializedKey = new VibeKeyObject(key);
-            Assert.Equal(key, preSerializedKey.Name); //* name should match string
-
-            string json = JsonConvert.SerializeObject(preSerializedKey);
-            JObject jObject = JObject.Parse(json);
-
-            Assert.True(jObject.Count == 1, "VibeKeyObject Json should only have one property: " + Json.NAME_KEY);
-            Assert.True(jObject[Json.NAME_KEY] != null, "VibeKeyObject Json's property should be named: " + Json.NAME_KEY);
-            Assert.Equal(key, jObject[Json.NAME_KEY].ToString()); //*Name should be the same
-        }
-
-        [Theory]
-        [InlineData("vibekey")]
-        [InlineData("")]
-        [InlineData(VibeKey.INVALID_KEY_NAME)]
-        public void Test_VibeKeyObjectDeserialization(string key)
-        {
-            string json = JsonConvert.SerializeObject(new VibeKeyObject(key));
-            var vibeKey = JsonConvert.DeserializeObject<VibeKeyObject>(json);
-
-            Assert.Equal(key, vibeKey.Name);
-        }
-
-        #endregion
     }
+
+    #endregion
 }
